2c2c1364cceab76aebdcbea2589604faffc1722b
bdarmawan <darmawan_budi@yahoo.com>
2020-10-16 15:50:41 -0700

    cleanup

--- tree/creatingBinaryTree.py
+++ tree/creatingBinaryTree.py
@@ -5,19 +5,16 @@ class Node:
         self.data  = data
 
     def insert(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right = self.right.insert(data)
-        else:
-            self.data = data
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right = self.right.insert(data)
 
 
     def printTree(self):
