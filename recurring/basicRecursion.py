1900f5039567d824ce0222f6285499581dcfad37
bdarmawan <darmawan_budi@yahoo.com>
2020-10-27 18:43:42 -0700

    basic recursion

---
+++ recurring/basicRecursion.py
@@ -0,0 +1,35 @@
+def fact(i):
+    if i == 1:
+        print (i, 1)
+        return 1
+    else:
+        x = i * fact(i-1)
+        print(i, x)
+        return x
+
+
+def fib(i):
+    if i == 0:
+        return 0
+    if i == 1:
+        return 1
+    else:
+        return fib(i-1) + fib(i-2)
+
+
+# fib with memoization - optimized
+def fib2(i, cache={}):
+   if i in cache:
+       return cache[i]
+   if i == 0 or i == 1:
+       return i
+   result = fib(i-1) + fib(i-2)
+   cache[i] = result
+   return result
+
+
+
+if __name__ == "__main__":
+    print(fact(5));
+    print(fib(5));
+    print(fib2(5));
