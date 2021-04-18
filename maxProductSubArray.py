71a470facebd086d42c3c2b7d342b76956abfd57
bdarmawan <darmawan_budi@yahoo.com>
2021-04-17 22:39:35 -0700

    new puzzle

---
+++ maxProductSubArray.py
@@ -0,0 +1,59 @@
+def maxProdSubarray(nums):
+    currMaxProduct = nums[0]
+    currMinProduct = nums[0]
+    prevMaxProduct = nums[0]
+    prevMinProduct = nums[0]
+    ans = nums[0]
+
+    for i in range(1, len(nums)):
+        currMaxProduct = max(prevMaxProduct * nums[i], prevMinProduct * nums[i], nums[i])
+        currMinProduct = min(prevMaxProduct * nums[i], prevMinProduct * nums[i], nums[i])
+        ans = max(currMaxProduct, currMinProduct)
+        prevMaxProduct = currMaxProduct
+        prevMinProduct = currMinProduct
+    return ans
+
+
+nums = [1,2,-3,0,6,7]
+print(f"Array: {nums}")
+print(maxProdSubarray(nums))
+
+nums = [1,2,-3,1,-6,7]
+print(f"Array: {nums}")
+print(maxProdSubarray(nums))
+
+nums = [1,2,-3,1,6,7]
+print(f"Array: {nums}")
+print(maxProdSubarray(nums))
+
+nums = [1,2,-3,0,6,-5]
+print(f"Array: {nums}")
+print(maxProdSubarray(nums))
+
+"""
+Array:        [1, 2,                   -3,                     0,  .................]
+    currMax    1  max(1*2, 1*2, 2)=2   max(2*-3, 2*-3, -3)=-6  max(-6*0, -6*0, 0)=0
+    currMin    1  min(1*2, 1*2, 2)=2   min(x*-3, 2*-3, -3)=-6  min(-6*0, -6*0, 0)=0
+    ans        1  max(2,2)=2           max(-6,-6)=-6           max(0,0)=0
+    prevMax    1  2                    -6                      0
+    prevMin    1  2                    -6                      0
+    
+Array:        [6,                      7]
+    currMax    max(0*6, 0*6, 6)=6      max(6*7, 6*7, 7)=42  
+    currMin    min(0*6, 0*6, 6)=6      min(6*7, 6*7, 7)=7   
+    ans        max(6,6)=6              max(42,7)=42          
+    prevMax    6                       42                      
+    prevMin    6                       7                      
+
+Array: [1, 2, -3, 0, 6, 7]
+42
+
+Array: [1, 2, -3, 1, -6, 7]
+252
+
+Array: [1, 2, -3, 1, 6, 7]
+42
+
+Array: [1, 2, -3, 0, 6, -5]
+0
+"""
---
+++ productOfArrayExceptSelf.py
@@ -0,0 +1,77 @@
+def prodArray(nums):
+    """
+    nums = [1, 2, 3, 4]
+    multiplyAll = 1*2*3*4 = 24
+    Now for each element, divide 24/nums[i]
+    op   = [24/1, 24/2, 24/3, 24/4] -> [24, 12, 8, 6]
+    O(n)
+    """
+    multiplyAll = 1
+    res = [0] * len(nums)
+    for i in range(len(nums)):
+        multiplyAll *= nums[i]
+
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            res[i] = 0
+        else:
+            res[i] = int(multiplyAll/nums[i])
+
+    return res
+
+
+def prodArray2(nums):
+    """
+    if division operation is not allowed.
+    nums  = [1,   2,  3,  4]
+    left  = [1,   2,  6, 24]  ::: this is LeftToRight
+    right = [24, 24, 12,  4]  ::: this is RightToLeft
+    boundaryCase:
+        * for i = 0   -> op[i] = right[1]
+        * for i = n-1 -> op[i] = left[n-2]
+    otherwise:
+        * op[i] = left[i-1] * right[i+1]
+    op    = [24, 1*12=12, 2*4=8, 6]
+    O(n)
+    """
+    left  = [1] * len(nums)
+    right = [1] * len(nums)
+    res   = [1] * len(nums)
+
+    n = len(nums)
+    for i in range(n):
+        if i == 0:
+            left[i] = nums[i]
+        else:
+            left[i] = left[i-1] * nums[i]
+    print(f"LEFT: {left}")
+
+    for i in range(n-1, -1, -1):
+        if i == n-1:
+            right[i] = nums[i]
+        else:
+            right[i] = right[i+1] * nums[i]
+    print(f"RIGHT: {right}")
+
+    for i in range(0, n):
+        if i == 0:
+            res[i] = right[1]
+        elif i == n-1:
+            res[i] = left[n-2]
+        else:
+            res[i] = left[i-1] * right[i+1]
+    return res
+
+
+array = [1,2,3,4]
+print(array)
+print(prodArray(array))
+print(prodArray2(array))
+
+"""
+[1, 2, 3, 4]
+[24, 12, 8, 6]
+LEFT: [1, 2, 6, 24]
+RIGHT: [24, 24, 12, 4]
+[24, 12, 8, 6]
+"""
