ed8d74639291e3d98ef0b5034477cbc6798fe254
bdarmawan <darmawan_budi@yahoo.com>
2021-09-27 14:12:45 -0700

    update

--- stack/check_parenthesis.py
+++ stack/check_parenthesis.py
@@ -1,3 +1,8 @@
+'''
+If the stack is empty at the end of the execution
+that means - all parenthesis are CHECKED
+'''
+
 def isValid(s):
     bracket_map = {"(": ")", "[": "]",  "{": "}"}
     open_par = set(["(", "[", "{"])
@@ -5,7 +10,7 @@ def isValid(s):
     for i in s:
         if i in open_par:
             stack.append(i)
-        elif stack and i == bracket_map[stack[-1]]:
+        elif stack  and  (i == bracket_map[stack[-1]]):
                 stack.pop()
         else:
             return False
@@ -14,12 +19,16 @@ def isValid(s):
 
 
 str = "(({{[[[]]]}}}))"
-print(f"{str} is valid: {isValid(str)}")
+print(f"{str} is valid: {isValid(str)}")    # False
+
+str = "(({{[[[]]]}}))"
+print(f"{str} is valid: {isValid(str)}")    # True
 
 str = "{[()}]"
-print(f"{str} is valid: {isValid(str)}")
+print(f"{str} is valid: {isValid(str)}")    # False
 
 """
 (({{[[[]]]}}})) is valid: False
+(({{[[[]]]}})) is valid: True
 {[()}] is valid: False
 """
