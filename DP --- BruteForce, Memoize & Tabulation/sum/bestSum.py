c40ceb8a0a9f93f3f70a2a5d90cf9d07253bbc91
bdarmawan <darmawan_budi@yahoo.com>
2021-10-06 09:46:37 -0700

    Update

--- DP --- BruteForce, Memoize & Tabulation/sum/bestSum.py
+++ DP --- BruteForce, Memoize & Tabulation/sum/bestSum.py
@@ -39,13 +39,35 @@ def bestSumMem(target, numbers, memo={}):
         remainder = target - number
         remainderCombination = bestSumMem(remainder, numbers, memo)
         if remainderCombination != None:
-            combination = remainderCombination + [number]    # Tricky !!!
+            combination = remainderCombination + [number]  # Tricky !!!
             if shortestAnswer == None  or  len(combination) < len(shortestAnswer):  # Tricky !!!
                 shortestAnswer = combination
 
     memo[target] = shortestAnswer
     return shortestAnswer
 
+
+### IF you need to know the count
+def bestSumMem2(target, numbers, memo={}):
+    if target in memo: return memo[target]
+    if target == 0: return []
+    if target < 0: return None
+
+    shortestAnswer = None
+    for number in numbers:
+        remainder = target - number
+        remainderCombination = bestSumMem(remainder, numbers, memo)
+        if remainderCombination != None:
+            if type(remainderCombination) == list:
+                combination = remainderCombination + [number]  # Tricky !!!
+            else:
+                combination = [remainderCombination] + [number]  # Tricky !!!
+
+            if shortestAnswer == None  or  len(combination) < len(shortestAnswer):  # Tricky !!!
+                shortestAnswer = combination
+
+    memo[target] = shortestAnswer
+    return len(shortestAnswer) if shortestAnswer != None else 0
 
 
 #######################################
@@ -75,6 +97,7 @@ def bestSumTab(targetSum, numbers):
 #####
 # 1 #
 #####
+print("bestSum")
 print(bestSum(8, [2, 3, 5, 10]))        # [5, 3]
 #Below will time out! b/c it takes so long to process - need to be optimized with Memoize
 #print(bestSum(100, [1, 2, 5, 25]))
@@ -83,15 +106,24 @@ print(bestSum(8, [2, 3, 5, 10]))        # [5, 3]
 #####
 # 2 #
 #####
+print("bestSumMem")
 print(bestSumMem(8, [2, 3, 5, 9, 10]))      # [5, 3]
 print(bestSumMem(100, [1, 2, 5, 25]))       # [25, 25, 25, 25]
 # print("============================")
 # print(f"'>> ' {bestSum(8, [2, 3, 5])}")
 # print("----------------------------")
-
+print("")
+print("bestSumMem2")
+print(bestSumMem2(8, [2, 3, 5, 9, 10],{}))      # 2
+print(bestSumMem2(100, [1, 2, 5, 24, 25],{}))   # 4
+print(bestSumMem2(100, [1, 2, 5, 25],{}))       # 4
+coins = [1, 3, 4, 5]
+amount = 7
+print(bestSumMem2(amount, coins,{}))            # 2 -> which is [4,3]
 
 #####
 # 3 #
 #####
+print("bestSumTab")
 print(bestSumTab(8, [2, 3, 5, 9, 10]))     # [3, 5]
 print(bestSumTab(100, [1, 2, 5, 25]))      # [25, 25, 25, 25]
