b01463f896ff020d161029eb200146641d8b7628
bdarmawan <darmawan_budi@yahoo.com>
2021-10-06 21:20:24 -0700

    merge two sorted linked list

---
+++ linked_list/merge_Two_LinkedList.py
@@ -0,0 +1,52 @@
+from typing import List
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        head = dummy
+        while (l1 != None and l2 != None):
+            if l1.val < l2.val:
+                dummy.next = l1
+                l1 = l1.next
+            else:
+                dummy.next = l2
+                l2 = l2.next
+            dummy = dummy.next  # Tricky -> make sure move dummy to the next one
+
+        # now check if there is any remainder
+        if l1 != None:
+            dummy.next = l1
+        else:
+            dummy.next = l2
+
+        return head.next    # because head is pointing to first dummy
+
+
+
+
+    def printList(self, head):
+        while head:
+            print(head.val, end = " ")
+            head = head.next
+
+###
+### TEST
+# l1 = 1 -> 2 -> 4
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+# l2 = 1 -> 3 -> 4
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+
+s = Solution()
+result = s.mergeTwoLists(l1, l2)     # OUTPUT: 1 1 2 3 4 4
+s.printList(result)
