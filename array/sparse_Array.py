aafcfe9767b6cd15946cc14cbb3fe3b5127d69d2
bdarmawan <darmawan_budi@yahoo.com>
2021-10-09 16:12:41 -0700

    sparse Array

---
+++ array/sparse_Array.py
@@ -0,0 +1,30 @@
+import os
+
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    strMap = {}
+    qMap = {}
+    for string in strings:
+        if string in strMap:
+            strMap[string] += 1
+        else:
+            strMap[string] = 1
+    print(strMap)
+
+    for query in queries:
+        if query in strMap:
+            qMap[query] = strMap[query]
+        else:
+            qMap[query] = 0
+    return qMap.values()
+
+
+if __name__ == '__main__':
+
+    strings = ['aba', 'baba', 'aba', 'xzxb']
+    queries= ['aba', 'xzxb', 'ab']
+
+    res = matchingStrings(strings, queries)
+
+    print('\n'.join(map(str, res)))
