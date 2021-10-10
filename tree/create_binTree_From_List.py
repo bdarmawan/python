019b4691abb33f850c77964cba6dc2f619446eb9
bdarmawan <darmawan_budi@yahoo.com>
2021-10-10 14:36:33 -0700

    Create Binary Tree from a List

---
+++ tree/create_binTree_From_List.py
@@ -0,0 +1,65 @@
+from typing import List
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+
+
+def createBinTree(myList: List[int]):
+    root = Node(myList[0])
+    for i in range(1, len(myList)):
+        insert(root, myList[i])
+    printTree(root)
+
+
+def insert(root, value):
+    if value < root.data:
+        if root.left is None:
+            node = Node(value)
+            root.left = node
+        else:
+            insert(root.left, value)
+    else:
+        if root.right is None:
+            node = Node(value)
+            root.right = node
+        else:
+            insert(root.right, value)
+
+
+def printTree(root):
+    if root.left is not None:
+        printTree(root.left)
+    print(root.data, end=" ")
+    if root.right is not None:
+        printTree(root.right)
+
+
+
+myList = [12, 6, 14, 3]
+createBinTree(myList)       #OUTPUT: 3 6 12 14
+'''
+            12
+          /     \
+        6         14
+      /  
+    3
+'''
+
+print("")
+myList = [12, 6, 14, 9, 3]
+createBinTree(myList)       #OUTPUT: 3 6 9 12 14
+'''
+            12
+          /     \
+        6         14
+      /   \
+    3      9
+'''
+
+print("")
+myList = [6, 14, 9, 3, 12]
+createBinTree(myList)       #OUTPUT: 3 6 9 12 14
