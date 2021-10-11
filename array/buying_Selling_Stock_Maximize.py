147f7c97810091c75ef8d9cdb805001f5010cb29
bdarmawan <darmawan_budi@yahoo.com>
2021-10-10 22:31:59 -0700

    Update

--- array/buying_Selling_Stock_Maximize.py
+++ array/buying_Selling_Stock_Maximize.py
@@ -1,36 +1,36 @@
 '''
-Each day, you can either buy one share of WOT, sell any number of shares
-or not make any transaction at all.
-What is the maximum profit you can obtain with an optimum trading strategy?
+On each day, you may decide to buy and/or sell the stock.
+You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
 '''
 
 def stockmax(prices):
-    res = []
-    for i in range(len(prices)):
-        profit = 0
-        buyPrice = -prices[i]
-        for j in range(i+1, len(prices)):
-            tmpProfit = buyPrice + prices[j]
-            profit = max(profit, tmpProfit)
-        res.append(profit)
-    print(res)
-    return sum(res)
+    profit = 0
 
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            profit += (prices[i] - prices[i-1])
+    return profit
 
 ###
 ### TEST
+prices = [7,1,5,3,6,4]
+print(stockmax(prices))     #OUTPUT : 7
+                            # (-1 + 5) + (-3 + 6)
 
 prices = [5, 3, 2]
 print(stockmax(prices))     #OUTPUT : 0
 
 prices = [1, 2, 100]
-print(stockmax(prices))     #OUTPUT : 197
-                            # (-1 + 2) + (-1 + 100)
-
+print(stockmax(prices))     #OUTPUT : 99
+                            # (-1 + 2) + (-2 + 100)
 prices =[1, 3, 1, 2]
 print(stockmax(prices))     #OUTPUT : 3
                             # (-1 + 3) + (-1 + 2)
 
 prices = [10, 22, 5, 75, 65, 80]
-print(stockmax(prices))     #OUTPUT : 3
-                            # (-1 + 3) + (-1 + 2)
+print(stockmax(prices))     #OUTPUT : 97
+                            # (-10 + 22) + (-5 + 75) + (-65 + 80)
+
