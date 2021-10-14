129c311126c654de246d7b9aada8f3f2ab837891
bdarmawan <darmawan_budi@yahoo.com>
2021-10-14 11:00:57 -0700

    Update

--- array/maxProduct_SubArray_ver1.py
+++ array/maxProduct_SubArray_ver1.py
@@ -8,7 +8,10 @@ def maxProdSubarray(nums):
     for i in range(1, len(nums)):
         currMaxProduct = max(prevMaxProduct * nums[i], prevMinProduct * nums[i], nums[i])
         currMinProduct = min(prevMaxProduct * nums[i], prevMinProduct * nums[i], nums[i])
-        ans = max(currMaxProduct, currMinProduct)
+        if i > 1:
+            ans = max(ans, currMaxProduct)
+        else:
+            anx = max(currMaxProduct, currMinProduct)
         prevMaxProduct = currMaxProduct
         prevMinProduct = currMinProduct
     return ans
@@ -16,26 +19,26 @@ def maxProdSubarray(nums):
 
 nums = [1,2,-3,0,6,7]
 print(f"Array: {nums}")
-print(maxProdSubarray(nums))
+print(maxProdSubarray(nums))        #OUTPUT: 42
 
 nums = [1,2,-3,1,-6,7]
 print(f"Array: {nums}")
-print(maxProdSubarray(nums))
+print(maxProdSubarray(nums))        #OUTPUT: 252
 
 nums = [1,2,-3,1,6,7]
 print(f"Array: {nums}")
-print(maxProdSubarray(nums))
+print(maxProdSubarray(nums))        #OUTPUT: 42
 
 nums = [1,2,-3,0,6,-5]
 print(f"Array: {nums}")
-print(maxProdSubarray(nums))
+print(maxProdSubarray(nums))        #OUTPUT: 6
 
 """
 Array:        [1, 2,                   -3,                     0,  .................]
-    currMax    1  max(1*2, 1*2, 2)=2   max(2*-3, 2*-3, -3)=-6  max(-6*0, -6*0, 0)=0
-    currMin    1  min(1*2, 1*2, 2)=2   min(x*-3, 2*-3, -3)=-6  min(-6*0, -6*0, 0)=0
-    ans        1  max(2,2)=2           max(-6,-6)=-6           max(0,0)=0
-    prevMax    1  2                    -6                      0
+    currMax    1  max(1*2, 1*2, 2)=2   max(2*-3, 2*-3, -3)=-3  max(-3*0, -6*0, 0)=0
+    currMin    1  min(1*2, 1*2, 2)=2   min(x*-3, 2*-3, -3)=-6  min(-3*0, -6*0, 0)=0
+    ans        1  max(2,2)=2           max(-3,-6)=-3           max(0,0)=0
+    prevMax    1  2                    -3                      0
     prevMin    1  2                    -6                      0
     
 Array:        [6,                      7]
