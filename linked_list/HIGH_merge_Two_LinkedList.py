22a3937bb6016f70de252cf9c6234a0ee3c30879
bdarmawan <darmawan_budi@yahoo.com>
2021-10-16 21:50:53 -0700

    Updated

--- linked_list/HIGH_merge_Two_LinkedList.py
+++ linked_list/HIGH_merge_Two_LinkedList.py
@@ -1,14 +1,15 @@
 from typing import List
 from typing import Optional
 
-class ListNode:
-    def __init__(self, val=0, next=None):
+class Node:
+    def __init__(self, val=0):
         self.val = val
-        self.next = next
+        self.next = None
+
 
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
+    def mergeTwoLists(self, l1: Optional[Node], l2: Optional[Node]) -> Optional[Node]:
+        dummy = Node(-1)
         head = dummy
         while (l1 != None and l2 != None):
             if l1.val < l2.val:
@@ -29,7 +30,6 @@ class Solution:
 
 
 
-
     def printList(self, head):
         while head:
             print(head.val, end = " ")
@@ -37,38 +37,38 @@ class Solution:
 
 
     def makeList(self, nums):
-        head = ListNode(nums[0])
+        head = Node(nums[0])
         cur = head
         for num in nums[1:]:
             while cur.next:
                 cur = cur.next
-            cur.next = ListNode(num)
+            cur.next = Node(num)
         return head
 
 ###
 ### TEST
-# l1 = 1 -> 2 -> 4
-# l1 = ListNode(1)
-# l1.next = ListNode(2)
-# l1.next.next = ListNode(4)
+# l1 = 1 -> 3 -> 5
+# l1 = Node(1)
+# l1.next = Node(3)
+# l1.next.next = Node(5)
 #
-# # l2 = 1 -> 3 -> 4
-# l2 = ListNode(1)
-# l2.next = ListNode(3)
-# l2.next.next = ListNode(4)
+# # l2 = 2 -> 4 -> 6
+# l2 = Node(1)
+# l2.next = Node(3)
+# l2.next.next = Node(4)
 
 
 
 s = Solution()
-list1 = [1,2,4]
+list1 = [1,3,5]
 l1 = s.makeList(list1)
 s.printList(l1)
 print("")
 
-list2 = [1,3,4]
+list2 = [2,4,6]
 l2 = s.makeList(list2)
 s.printList(l2)
 print("")
 
-result = s.mergeTwoLists(l1, l2)     # OUTPUT: 1 1 2 3 4 4
+result = s.mergeTwoLists(l1, l2)     # OUTPUT: 1 2 3 4 5 6
 s.printList(result)
