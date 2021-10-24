020d80bf3234922aa80d7d1cb254463c842123e2
bdarmawan <darmawan_budi@yahoo.com>
2021-10-23 20:14:28 -0700

    Updated

--- string/HIGH_plusOne.py
+++ string/HIGH_plusOne.py
@@ -1,6 +1,30 @@
 from typing import List    # this is needed for line #3 below (List[])
 
+###
+### 1st Option
 def plusOne(digits: List[int]) -> List[int]:
+    digits = digits[::-1]
+    carry = 1
+    i = 0
+
+    while carry and i < len(digits):
+        if digits[i] == 9:
+            digits[i] = 0
+            carry = 1
+        else:
+            digits[i] += 1
+            carry = 0
+        i += 1
+
+    if carry:
+        digits.append(1)
+
+    return digits[::-1]
+
+
+###
+### 2nd Option
+def plusOne_v2(digits: List[int]) -> List[int]:
     digits = digits[::-1]     # reverse the order
     carry, i = 1, 0
 
@@ -23,12 +47,16 @@ def plusOne(digits: List[int]) -> List[int]:
 ### TEST
 digits = [9,0,0]
 print(plusOne(digits))     # OUTPUT: [9, 0, 1]
+print(plusOne_v2(digits))     # OUTPUT: [9, 0, 1]
 
 digits = [9,9,9]
 print(plusOne(digits))     # OUTPUT: [1, 0, 0, 0]
+print(plusOne_v2(digits))     # OUTPUT: [1, 0, 0, 0]
 
 digits = [0]
 print(plusOne(digits))     # OUTPUT: [1]
+print(plusOne_v2(digits))     # OUTPUT: [1]
 
 digits = [9]
 print(plusOne(digits))     # OUTPUT: [1, 0]
+print(plusOne_v2(digits))     # OUTPUT: [1, 0]
